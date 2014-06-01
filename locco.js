e05f8d89aff1b7e10fa4c1c7cda08d44238c1669
Xavier Via <xavierviacanel@gmail.com>
2014-05-31 23:45:34 -0300

    Wording correction [skip ci]

--- locco.js
+++ locco.js
@@ -49,9 +49,9 @@ var mustache  = require("mustache")
 // > **locco** does everything synchronously. Why is that?
 // > Because locco is useful in contexts where asynchronicity means nothing
 // > but troble.
-// > Doc generation is intended to be atomic for each file in locco. Files
+// > In **locco**, doc generation for each file is intended to be atomic. Files
 // > are usually too many for you to be able to handle callbacks without
-// > serious acrobatics, and with no real benefit, we should you?
+// > serious acrobatics, and with no real benefit, why should you?
 //
 // #### Options
 //
