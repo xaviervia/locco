81ef612a498d3269658d73fc7934e28b33d79d95
Xavier Via <fernando.via@gmail.com>
2014-11-16 00:50:05 +0100

    Add CI status

---
+++ example/code.md
@@ -0,0 +1,4 @@
+This is a comment
+=================
+
+That will be output as _markdown_, eventually
--- locco.js
+++ locco.js
@@ -1,6 +1,8 @@
 // locco
 // =====
 //
+// ![Codeship status](https://www.codeship.io/projects/173f7bd0-ad2d-0131-d326-5a3e053281b1/status)
+//
 // Simple documentation extractor.
 //
 var spec              = require("washington")
